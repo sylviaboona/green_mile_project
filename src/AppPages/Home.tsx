67c42c2f906b3b782f1e8b06a2ca6d7f64291f69
Sylvia Boona <sylvieboona@gmail.com>
2021-06-16 18:52:57 +0300

    home page route

--- src/AppPages/Home.tsx
+++ src/AppPages/Home.tsx
@@ -5,10 +5,8 @@ const Home = () => {
     return (
         <div className="login-left-panel">
             <NavBar />
-            <div className="login-left-panel">
-                <p className="tagline-loginpage">Your number one delivery partner</p>
+            <p className="tagline-loginpage">Your number one delivery partner</p>
                 {/* <img src={doorDelivery} alt="Door Delivery img" className="login-left-panel-img" /> */}
-            </div>
         </div>
     )
 }
