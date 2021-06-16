9e6527609244ad31776b5d56f3ae0de338c363fd
Sylvia Boona <sylvieboona@gmail.com>
2021-06-16 18:48:38 +0300

    home page route

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 const NavBar = () => {
     return (
         <div className="navbar">
-            <Link to="/">HOME</Link>
+            {/* <Link to="/">HOME</Link> */}
             <Link to="/addLoader" className="navbar-link">ADD LOADER</Link>
             <Link to="/login" className="navbar-link">LOGIN</Link>
             
