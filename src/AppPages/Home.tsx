994b46708f573b2539f2bcbbe775d2d3d929d30b
Sylvia Boona <sylvieboona@gmail.com>
2021-06-16 18:41:08 +0300

    home page route

--- src/AppPages/Home.tsx
+++ src/AppPages/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
 
 const Home = () => {
     return (
@@ -6,7 +7,7 @@ const Home = () => {
             <NavBar />
             <div className="login-left-panel">
                 <p className="tagline-loginpage">Your number one delivery partner</p>
-                <img src={doorDelivery} alt="Door Delivery img" className="login-left-panel-img" />
+                {/* <img src={doorDelivery} alt="Door Delivery img" className="login-left-panel-img" /> */}
             </div>
         </div>
     )
