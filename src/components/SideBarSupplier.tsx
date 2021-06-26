3d54532633b33ba5529db4b5a54b96af1d4fe60c
Sylvia Boona <sylvieboona@gmail.com>
2021-06-26 15:51:13 +0300

    supplier profile photo

--- src/components/SideBarSupplier.tsx
+++ src/components/SideBarSupplier.tsx
@@ -6,37 +6,36 @@ import SettingsIcon from '@material-ui/icons/Settings';
 // import ReceiptIcon from '@material-ui/icons/Receipt';
 import ShoppingCartSharpIcon from '@material-ui/icons/ShoppingCartSharp';
 import CardGiftcardSharpIcon from '@material-ui/icons/CardGiftcardSharp';
+import gmlogo from '../assets/img/profile3.jpg';
 const SideBarSupplier = () => {
     return (
-<div className="side-bar">
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
+        <div className="side-bar">
+             <img src={gmlogo} width="250" alt="GreenMileLogo" className="profile-image-supplier" />
+        <br />
+        <div >
+            <br />
+            <Link to="/supplierDash" className="sidebar-link"><DashboardIcon />  My Account</Link>
             <br />
             <br />
-            <Link to="/supplierDash" className="sidebar-link"><DashboardIcon/>  My Account</Link>
-            <br/>
-            <br/>
-            <br/>
-            <Link to="/viewPackages" className="sidebar-link"> <CardGiftcardSharpIcon/>   View Packages</Link>
-            <br/>
-            <br/>
-            <br/>
-            <Link to="/viewOrders" className="sidebar-link"><ShoppingCartSharpIcon/>   View Orders</Link>
-            <br/>
-            <br/>
-            <br/>
-            <Link to="/viewReceipients" className="sidebar-link"><ViewListIcon/>   View Receipients</Link>
+            <br />
+            <Link to="/viewPackages" className="sidebar-link"> <CardGiftcardSharpIcon />   View Packages</Link>
+            <br />
+            <br />
+            <br />
+            <Link to="/viewOrders" className="sidebar-link"><ShoppingCartSharpIcon />   View Orders</Link>
+            <br />
+            <br />
+            <br />
+            <Link to="/viewReceipients" className="sidebar-link"><ViewListIcon />   View Receipients</Link>
             {/* <br/>
             <br/>
             <br/>
             <Link to="/viewInvoices" className="sidebar-link">View Invoices</Link> */}
-            <br/>
-            <br/>
-            <br/>
-            <Link to="/settings" className="sidebar-link"> <SettingsIcon/>    Settings</Link>
+            <br />
+            <br />
+            <br />
+            <Link to="/settings" className="sidebar-link"> <SettingsIcon />    Settings</Link>
+        </div>
         </div>
     )
 }
